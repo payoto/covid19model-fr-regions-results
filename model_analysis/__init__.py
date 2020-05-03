614eed5f88306529b6ec659e5bcecc39981f0b87
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-05-03 19:00:54 +0200

    Actually separate interface and core
    Checked functionality by running the notebooks. I should really use
    nbdev.

--- model_analysis/__init__.py
+++ model_analysis/__init__.py
@@ -7,13 +7,21 @@ from .data_plot import (
     plot_forecast_countries, plot_forecast_country, 
     plot_model_countries, plot_model_country, 
     plot_report_country, plot_report_countries,
+    plot_daily_deaths_countries, plot_daily_deaths_country,
     compare_fatality_predictions,
+)
+
+from .plot_core import (
     get_next_color,
     remove_confidence_interval_lines,
     remove_confidence_interval_legend_labels,
     modify_legend,
+    axis_date_limits,
+    define_new_cycle,
+    default_color_cycle,
 )
 
 from . import data_plot as icl_plot
+from . import plot_core as icl_plot_core
 from . import data_load as icl_load
                     
--- model_analysis/plot_core.py
+++ model_analysis/plot_core.py
@@ -6,6 +6,10 @@ from matplotlib.rcsetup import cycler
 # Marks a confidence interval
 _ci_mark = "CI_"
 
+confidence_interval_format = {
+    "linestyle": ':',
+    "marker": '',
+}
 # Collect the error behaviour of pandas when plotting an empty dataframe
 error_pd_empty_plot = Exception()
 try:
@@ -14,10 +18,6 @@ except Exception as e:
     error_pd_empty_plot = e
 type(error_pd_empty_plot)
 
-confidence_interval_format = {
-    "linestyle": ':',
-    "marker": '',
-}
 default_color_cycle = plt.rcParams['axes.prop_cycle']
 # Defines extensions to matplotlib for convenience
 
@@ -124,150 +124,6 @@ def define_new_cycle(
         )
 
     return prop_cycle_sized
-
-# plots the 
-
-def plot_forecast_country(
-    forecast_df,
-    country,
-    *,
-    country_label=None,
-    **kwargs
-):
-    """ Plots the forecasted daily deaths with the 95% confidence interval.
-
-    Arguments:
-        forecast_df (pd.Dataframe) : a data frame whic results from loading
-        <run-name>-forecast-data.csv
-        country (str) : a string corresponding to a country in the 
-
-    kwargs are passed to `pd.Dataframe.plot()`:
-        color (str): A color hex string
-        ax (plt.Axes) : An axis 
-    """
-    # Select the active columns
-    active_columns = [
-        col for col in forecast_df.columns if 'estimated_deaths' in col
-    ]
-    plot_quantity = "Forecasted daily deaths"
-    date_label = min(forecast_df["time"])
-    
-    if country_label is None:
-        country_label = f"{country} (forecast from {date_label})"
-    country_field = "country"
-
-    return plot_timeseries_confidence_interval_country(
-        forecast_df,
-        active_columns,
-        plot_quantity, # y label of the plot
-        country,
-        country_label=country_label,
-        country_field=country_field,
-        timeseries_type="forecast",
-        **kwargs
-    )
-
-
-def plot_model_country(
-    model_df,
-    country,
-    *,
-    country_label=None,
-    **kwargs
-):
-    """ Plots the forecasted daily deaths with the 95% confidence interval.
-
-    Arguments:
-        model_df (pd.Dataframe) : a data frame whic results from loading
-        <run-name>-forecast-data.csv
-        country (str) : a string corresponding to a country in the 
-
-    kwargs are passed to `pd.Dataframe.plot()`:
-        color (str): A color hex string
-        ax (plt.Axes) : An axis 
-    """
-    # Select the active columns
-    active_columns = [
-        "estimated_deaths", "death_min", "death_max",
-    ]
-    plot_quantity = "Modelled daily deaths"
-    date_label = max(model_df["time"])
-    country_field="region"
-    if country_label is None:
-        country_label = f"{country} (model to {date_label})"
-    
-    return plot_timeseries_confidence_interval_country(
-        model_df,
-        active_columns, 
-        plot_quantity, # y label of the plot
-        country,
-        country_label=country_label,
-        country_field=country_field,
-        timeseries_type="model",
-        **kwargs
-    )
-
-
-def plot_report_country(
-    model_df,
-    country,
-    *,
-    country_label=None,
-    **kwargs
-):
-    """ Plots the forecasted daily deaths with the 95% confidence interval.
-
-    Arguments:
-        model_df (pd.Dataframe) : a data frame whic results from loading
-        <run-name>-forecast-data.csv
-        country (str) : a string corresponding to a country in the 
-
-    kwargs are passed to `pd.Dataframe.plot()`:
-        color (str): A color hex string
-        ax (plt.Axes) : An axis 
-    """
-    # Select the active columns
-    active_columns = [
-        "deaths",
-    ]
-    plot_quantity = "Reported daily deaths"
-    date_label = max(model_df["time"])
-    country_field="region"
-    if country_label is None:
-        country_label = f"{country} (Reported deaths to {date_label})"
-    
-    return plot_timeseries_country(
-        model_df,
-        active_columns, 
-        plot_quantity, # y label of the plot
-        country,
-        country_label=country_label,
-        country_field=country_field,
-        timeseries_type="reports",
-        **kwargs
-    )
-
-def plot_daily_deaths_country(
-    data_dict,
-    country,
-    *,
-    ax=None,
-    **kwargs
-):
-    if ax is None:
-        fig, ax = plt.subplots()
-    labels = ['', '', '']
-    ax, labels[0] = plot_report_country(
-        data_dict["modelling"], country, ax=ax, **kwargs
-    )
-    ax, labels[1] = plot_model_country(
-        data_dict["modelling"], country, ax=ax, **kwargs
-    )
-    ax, labels[2] = plot_forecast_country(
-        data_dict["forecasting"], country, ax=ax, **kwargs
-    )
-    remove_confidence_interval_legend_labels(ax)
-    return ax, labels 
 
 def plot_timeseries_confidence_interval_country(
     forecast_df,
@@ -420,19 +276,6 @@ def enable_time_series_plot(
     return in_df
 
 
-def plot_forecast_countries(*args, **kwargs):
-    return plot_timeseries_countries(plot_forecast_country, *args, **kwargs)
-
-def plot_report_countries(*args, **kwargs):
-    return plot_timeseries_countries(plot_report_country, *args, **kwargs)
-
-def plot_model_countries(*args, **kwargs):
-    return plot_timeseries_countries(plot_model_country, *args, **kwargs)
-    
-def plot_daily_deaths_countries(*args, **kwargs):
-    return plot_timeseries_countries(plot_daily_deaths_country, *args, **kwargs)
-
-
 def plot_timeseries_countries(
     plot_timeseries_func,
     forecast_df, 
@@ -471,53 +314,3 @@ def plot_timeseries_countries(
     remove_confidence_interval_legend_labels(axout)
     
     return axout
-
-def compare_fatality_predictions(
-    data_dict,
-    country_list=None,
-    ax=None,
-    prop_cycle=None,
-    max_num_country_ci_display=3,
-    plot_specific_kwargs={},
-    **kwargs
-    ):
-    if prop_cycle is None:
-        # define a new property cycle to ensure the colour repeats correctly
-        prop_cycle = define_new_cycle(
-            marker=['o',',',','], # which varies the marker
-            color_frequency=len(country_list), # the same colour repeats every len(country_list) lines
-            markevery=[5],  # Markers are plotted every 5 days
-        )
-    if ax is None:
-        _, ax = plt.subplots()
-        ax.set_prop_cycle(prop_cycle)
-    
-    # Get the correct precedence of keyword args
-    plot_kwargs = {
-        "report": {'linestyle':'', 'markevery':1, 'marker': 'o'},
-        "model": {},
-        "forecast": {'linestyle':'--'},
-    }
-    for plot in plot_kwargs:
-        for kwarg in kwargs:
-            plot_kwargs[plot][kwarg] = kwargs[kwarg]
-    for plot in plot_specific_kwargs:
-        for kwarg in plot_specific_kwargs[plot]:
-            plot_kwargs[plot][kwarg] = plot_specific_kwargs[plot][kwarg]
-
-    # Plot the data (the reported data without any line, and with a marker every time)
-    plot_report_countries(data_dict["modelling"], country_list=country_list,
-        ax=ax, **plot_kwargs["report"])
-    plot_model_countries(data_dict["modelling"], country_list=country_list,
-        ax=ax, **plot_kwargs["model"])
-    plot_forecast_countries(data_dict["forecasting"], country_list=country_list,
-        ax=ax, **plot_kwargs["forecast"])
-
-    # For clarity we remove the confidence intervals and the move the legend out
-    if max_num_country_ci_display >= len(country_list):
-        remove_confidence_interval_legend_labels(ax)
-    else:
-        remove_confidence_interval_lines(ax)
-
-    modify_legend(ax, bbox_to_anchor=(1.04, 1.0), loc='upper left')
-    return ax
