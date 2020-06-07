1266f0bc7492e910e6db59068ed1c2ceb8464090
Alexandre Payot <alexandre.payotli@gmail.com>
2020-06-07 11:37:41 +0200

    Start mapping data

--- model_analysis/translate.py
+++ model_analysis/translate.py
@@ -1,4 +1,6 @@
 import locale
+import pylcs
+import numpy as np
 # locale.setlocale(locale.LC_ALL, "")
 
 def translate_label(label):
@@ -51,3 +53,36 @@ def translate_axes_to_french(axs):
     for child in axs[0].figure.get_children():
         if hasattr(child, "set_text") and hasattr(child, "get_text"):
             child.set_text(translate_label(child.get_text()))
+
+def  most_probable_match(string, possible_strings, n_matches=1):
+    """ Fuzzy lookup for a similar string
+    """
+
+    def return_match(arg_most_prob, probable_match, i_match=1):
+        print(
+            f"\tMatch {i_match} of {n_matches} : "
+            + f"'{possible_strings[arg_most_prob]}' == '{string}'  "
+            + f"with a match score of {probable_match[arg_most_prob]}/{4*len(string)}."
+        )
+        return possible_strings[arg_most_prob]
+
+    possible_strings_l = [s.lower() for s in possible_strings]
+    string_l = string.lower()
+    lcsubsequence_list_lower = pylcs.lcs_of_list(string_l, possible_strings_l)
+    probable_match = (np.add(np.add(
+        pylcs.lcs_of_list(string, possible_strings),  # longest subsequence 
+        pylcs.lcs2_of_list(string, possible_strings) # longest substring
+    ), np.add(
+        lcsubsequence_list_lower,  # longest subsequence case insensitive
+        pylcs.lcs2_of_list(string_l, possible_strings_l) # longest substring case insensitive
+    )).astype(np.int) - (
+        np.array([2*(len(t) - m) for t, m in zip(possible_strings_l, lcsubsequence_list_lower)])  # Penalise extra characters in the match
+    ))
+    if n_matches == 1:
+        arg_most_prob = np.argmax(probable_match)
+        return return_match(arg_most_prob, probable_match)
+    elif n_matches > 1:
+        args_most_prob = np.argpartition(probable_match, -n_matches)[-1:-n_matches-1:-1]
+        return [return_match(a, probable_match, i+1) for i, a in enumerate(args_most_prob)]
+    else: 
+        raise AttributeError("Invalid number of matches requested")
